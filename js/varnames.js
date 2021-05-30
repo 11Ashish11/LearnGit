930da560bb14acd54b9d36d38fddbb14ab97a2dd
Ashish <ashish.bhoya.56@gmail.com>
2021-05-31 00:10:56 +0530

    added programming names specifying types

--- js/varnames.js
+++ js/varnames.js
@@ -5,3 +5,26 @@ console.log("This is the varible session")
 var name  = "Ashish"
 var company = "whatfix"
 console.log(name , company)
+
+//var was used more often in the old javascript
+//in the new javasciprt just const and let are used
+
+//const as the name suggests 
+//once anything marked with const , it cannot be changed later
+
+//as of now  , let is almost same as var only
+
+const arr = [10,20,30,40,50]
+arr.push(20)
+arr[0] = 20
+console.log(arr)
+
+/* Most common programming type cases
+
+
+1. camelCase
+2. kebab-case
+3. snake_case
+4. PascalCase
+
+*/
